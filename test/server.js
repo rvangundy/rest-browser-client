d46d4cb62dd2307097bbe68bdc64accc167e267e
Ryan VanGundy <ryanvangundy@Ryans-MacBook-Pro.local>
2013-12-28 18:19:33 -0500

    Add error test target

--- test/server.js
+++ test/server.js
@@ -9,5 +9,10 @@ server.get('/', cors(), function(req, res) {
     res.send('test');
 });
 
+server.get('/error', cors(), function(req, res) {
+    res.status(404);
+    res.send();
+});
+
 server.listen(port);
 console.log('listening on port ' + port);
