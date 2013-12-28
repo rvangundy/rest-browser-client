d5ee9f52637abe51873c172c05e6695736a02f3c
Ryan VanGundy <ryanvangundy@Ryans-MacBook-Pro.local>
2013-12-28 18:19:54 -0500

    Testing 'get' functionality

--- test/test.js
+++ test/test.js
@@ -6,12 +6,22 @@ var assert = chai.assert;
 require('./shim-function.bind');
 
 describe('Client', function () {
-    var client = rest('http://localhost:8000');
 
-    it('performs a basic get operation', function (ok) {
-        client.get('', function(req) {
-            assert.equal(req.response, 'test');
-            ok();
+    describe('get', function() {
+        it('Performs a basic get operation', function (ok) {
+            var client = rest('http://localhost:8000');
+            client.get(function(req) {
+                ok();
+        });
+        it('Calls error middleware', function(ok) {
+            var message = 'some error';
+
+                assert.equal(err.status, 404);
+                ok();
+            });
+        });
+    });
+
         });
     });
 });
